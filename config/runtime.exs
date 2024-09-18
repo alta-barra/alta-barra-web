eb95660f684f0ec7e4e73d57a7c0ee60cd297a12
Jay Barra <jay.barra@draconic-systems.com>
2024-09-18 12:35:21 -0400

    Update ssl runtime options

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,11 +38,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :altabarra, Altabarra.Repo,
-    ssl: true,
-    ssl_opts: [
-      verify: :verify_none,
-      cacertfile: "/etc/ssl/certs/rds-combined-ca-bundle.pem"
-    ],
+    ssl: [cacertfile: "/etc/ssl/certs/rds-combined-ca-bundle.pem"],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
