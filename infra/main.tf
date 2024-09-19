c1ccf111cf8165cd89963348b60cbb22243edcf7
Jay Barra <jay.barra@draconic-systems.com>
2024-09-19 18:15:55 -0400

    Remove duplicate security group https mapping

--- infra/main.tf
+++ infra/main.tf
@@ -626,15 +626,6 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ephemeral_http_in" {
   to_port     = 65535
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_https_in" {
-  security_group_id = aws_security_group.alb.id
-
-  cidr_ipv4   = var.vpc_cidr_block
-  from_port   = 443
-  ip_protocol = "tcp"
-  to_port     = 443
-}
-
 resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_http_in" {
   security_group_id = aws_security_group.alb.id
 
