8c46e7e0658a462c0c0597f2c8109afd0cb1a2b9
Jay Barra <jay.barra@draconic-systems.com>
2024-09-19 18:13:47 -0400

    Add cidr_block to ingress security groups

--- infra/main.tf
+++ infra/main.tf
@@ -620,6 +620,7 @@ resource "aws_security_group" "ec2" {
 resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ephemeral_http_in" {
   security_group_id = aws_security_group.alb.id
 
+  cidr_ipv4   = var.vpc_cidr_block
   from_port   = 1024
   ip_protocol = "tcp"
   to_port     = 65535
@@ -628,6 +629,7 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ephemeral_http_in" {
 resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_https_in" {
   security_group_id = aws_security_group.alb.id
 
+  cidr_ipv4   = var.vpc_cidr_block
   from_port   = 443
   ip_protocol = "tcp"
   to_port     = 443
@@ -636,6 +638,7 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_https_in" {
 resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_http_in" {
   security_group_id = aws_security_group.alb.id
 
+  cidr_ipv4   = var.vpc_cidr_block
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
@@ -644,6 +647,7 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_http_in" {
 resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ssh_in" {
   security_group_id = aws_security_group.bastion_host.id
 
+  cidr_ipv4   = var.vpc_cidr_block
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
