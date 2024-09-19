46d847a06b52a58445b025368e1033248e7e3a79
Jay Barra <jay.barra@draconic-systems.com>
2024-09-19 17:54:05 -0400

    Pass secret_key_base in

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,31 +1,8 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/altabarra start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
   config :altabarra, AltabarraWeb.Endpoint, server: true
 end
-
-# config :logger,
-#   backends: [:console, {LoggerFileBackend, :error_log}]
-
-# config :logger, :error_log,
-#   path: "/var/log/phoenix_app.log",
-#   level: :info
 
 if config_env() == :prod do
   database_url =
@@ -61,69 +38,10 @@ if config_env() == :prod do
   config :altabarra, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :altabarra, AltabarraWeb.Endpoint,
-    url: [host: host, port: port, scheme: "http"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
-    ]
-
-  # ,
-  #   https: [
-  #     port: 443,
-  #     cipher_suite: :strong,
-  #     keyfile: System.get_env("SSL_KEY_PATH"),
-  #     certfile: System.get_env("SSL_CERT_PATH")
-  #   ],
-  #   force_ssl: [hsts: true],
-  #   secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :altabarra, AltabarraWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :altabarra, AltabarraWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :altabarra, Altabarra.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+    ],
+    secret_key_base: secret_key_base
 end
