0bd44183f48ef9e7a71ed8927153c31ad4756e9c
Jay Barra <jay.barra@draconic-systems.com>
2024-09-19 18:11:54 -0400

    Replace ingress rules with newer versions

--- infra/main.tf
+++ infra/main.tf
@@ -612,33 +612,50 @@ resource "aws_security_group" "ec2" {
   description = "Security group for EC2 instances in ECS cluster"
   vpc_id      = aws_vpc.default.id
 
-  ingress {
-    description     = "Allow ingress traffic from ALB on HTTP on ephemeral ports"
-    from_port       = 1024
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
-
-  ingress {
-    description     = "Allow SSH ingress traffic from bastion host"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_host.id]
-  }
-
-  egress {
-    description = "Allow all egress traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "${var.namespace}_EC2_Instance_SecurityGroup_${var.environment}"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ephemeral_http_in" {
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 1024
+  ip_protocol = "tcp"
+  to_port     = 65535
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_https_in" {
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_http_in" {
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_bastion_ssh_in" {
+  security_group_id = aws_security_group.bastion_host.id
+
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_out" {
+  security_group_id = aws_security_group.ec2.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 0
+  ip_protocol = "tcp"
+  to_port     = 0
 }
 
 resource "aws_security_group" "alb" {
