89ef5cbf4200b20b429c162fffdc1d3c5a372fca
Jay Barra <jay.barra@draconic-systems.com>
2024-09-20 11:18:31 -0400

    Remove unused cloudfront certificate

--- infra/main.tf
+++ infra/main.tf
@@ -42,17 +42,6 @@ resource "aws_acm_certificate_validation" "alb_certificate" {
   validation_record_fqdns = [aws_route53_record.generic_certificate_validation.fqdn]
 }
 
-resource "aws_acm_certificate" "cloudfront_certificate" {
-  domain_name               = var.domain_name
-  validation_method         = "DNS"
-  subject_alternative_names = ["*.${var.domain_name}"]
-}
-
-resource "aws_acm_certificate_validation" "cloudfront_certificate" {
-  certificate_arn         = aws_acm_certificate.cloudfront_certificate.arn
-  validation_record_fqdns = [aws_route53_record.generic_certificate_validation.fqdn]
-}
-
 resource "aws_route53_record" "generic_certificate_validation" {
   name    = tolist(aws_acm_certificate.alb_certificate.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.alb_certificate.domain_validation_options)[0].resource_record_type
