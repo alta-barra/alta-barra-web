4c98c662d0f3d09a322a254e2e4e0b98d81063be
Jay Barra <jay.barra@draconic-systems.com>
2024-10-01 11:11:52 -0400

    Update entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Ensure required environment variables are set
-: "${DB_USERNAME:?Need to set DB_USERNAME}"
-: "${DB_PASSWORD:?Need to set DB_PASSWORD}"
-: "${DB_HOST:?Need to set DB_HOST}"
-: "${DB_NAME:?Need to set DB_NAME}"
-
 # Construct the DATABASE_URL
 export DATABASE_URL="ecto://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}"
 
