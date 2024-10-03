3c789af9526d709a8776e4ce96e0e636627603d5
Jay Barra <jay.barra@draconic-systems.com>
2024-10-03 09:36:44 -0400

    Remove unused resources from app infra

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -565,51 +565,6 @@ resource "aws_security_group" "bastion_host" {
   }
 }
 
-resource "aws_s3_bucket" "elixir_app_bucket" {
-  bucket = "alta-barra-elixir-app-deployments-bucket-${var.environment}"
-  acl    = "private"
-}
-
-resource "aws_iam_role" "ec2_s3_access_role" {
-  name = "ec2_s3_access_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com",
-        },
-      },
-    ],
-  })
-}
-
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "s3_access_policy"
-  description = "Allow EC2 instances to access S3 bucket"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = ["s3:GetObject", "s3:ListBucket"],
-        Effect = "Allow",
-        Resource = [
-          "${aws_s3_bucket.elixir_app_bucket.arn}",
-          "${aws_s3_bucket.elixir_app_bucket.arn}/*",
-        ],
-      },
-    ],
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_attach_s3_access" {
-  role       = aws_iam_role.ec2_s3_access_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
 resource "aws_instance" "elixir_app_server" {
   count                       = 1
   ami                         = data.aws_ami.amazon_linux_2_free_tier.id
