2c8f9eba8473ee49f58bb041bf5b9f369a3f6f0f
Jay Barra <jay.barra@draconic-systems.com>
2024-10-09 13:16:58 -0400

    Set environment variables for the ECS tasks

--- infra/main.tf
+++ infra/main.tf
@@ -64,7 +64,7 @@ module "ecs" {
       managed_termination_protection = "ENABLED"
 
       managed_scaling = {
-        maximum_scaling_step_size = 5
+        maximum_scaling_step_size = 1
         minimum_scaling_step_size = 1
         status                    = "ENABLED"
         target_capacity           = 60
@@ -110,17 +110,23 @@ module "ecs_service" {
         }
       ]
 
+      environment = [
+        {
+          name  = "DB_USERNAME",
+          value = "altabarra"
+        },
+        {
+          name  = "DB_PASSWORD",
+          value = "altabarra"
+        }
+      ]
+
       mount_points = [
         {
           sourceVolume  = "my-vol",
           containerPath = "/var/www/my-vol"
         }
       ]
-
-      #entry_point = ["/usr/sbin/apache2", "-D", "FOREGROUND"]
-
-      # Example image used requires access to write to root filesystem
-      readonly_root_filesystem = false
 
       enable_cloudwatch_logging              = true
       create_cloudwatch_log_group            = true
